3eb6cf73cae41c4f0c1b325696e6890b7c9bafe9
IrfanNaikwade28 <irfannayakvade2230@gmail.com>
2026-08-02 11:18:45 +0530

    feat: added URI to contact button

--- frontend/src/components/business/BusinessInfo.jsx
+++ frontend/src/components/business/BusinessInfo.jsx
@@ -8,9 +8,7 @@ export const BusinessInfo = ({ business }) => {
     <div className="mt-2 rounded-2xl border border-gray-200 bg-back-secondary p-5">
       {/* Business Name */}
       <div>
-        <h2 className="text-2xl font-bold text-primary">
-          {business.name}
-        </h2>
+        <h2 className="text-2xl font-bold text-primary">{business.name}</h2>
 
         <p className="mt-2 text-sm leading-6 text-primary font-light">
           {business.address.full}
@@ -19,10 +17,13 @@ export const BusinessInfo = ({ business }) => {
 
       {/* Actions */}
       <div className="mt-5 flex gap-3">
-        <button className="flex items-center gap-2 rounded-lg border border-gray-200 bg-white px-4 py-2 text-sm text-primary">
+        <a
+          href={`tel:${business.contact.phone}`}
+          className="flex items-center gap-2 rounded-lg border border-gray-200 bg-white px-4 py-2 text-sm text-primary transition-all duration-150 active:scale-105 active:bg-gray-100"
+        >
           <Phone className="h-4 w-4" />
           Contact Restaurant
-        </button>
+        </a>
       </div>
 
       {/* Opening Status */}
@@ -45,9 +46,7 @@ export const BusinessInfo = ({ business }) => {
       {/* Legal Information */}
       <div className="space-y-4">
         <div>
-          <p className="text-xs text-primary font-extralight">
-            Legal Name
-          </p>
+          <p className="text-xs text-primary font-extralight">Legal Name</p>
 
           <h3 className="text-base font-semibold text-primary">
             {business.legal.name}
@@ -55,9 +54,7 @@ export const BusinessInfo = ({ business }) => {
         </div>
 
         <div>
-          <p className="text-xs text-primary font-extralight">
-            GST Number
-          </p>
+          <p className="text-xs text-primary font-extralight">GST Number</p>
 
           <h3 className="text-base font-semibold text-primary">
             {business.legal.gstNumber}
