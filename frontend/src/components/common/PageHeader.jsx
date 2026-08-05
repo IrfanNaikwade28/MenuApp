78c49ad424664a97d8fb7af2a8418ce094f7375a
IrfanNaikwade28 <irfannayakvade2230@gmail.com>
2026-08-05 19:01:46 +0530

    fix: pageHeader navigate method

--- frontend/src/components/common/PageHeader.jsx
+++ frontend/src/components/common/PageHeader.jsx
@@ -1,14 +1,18 @@
 import { useNavigate } from "react-router-dom";
 import { ArrowLeft } from "../../constants/icons";
-export const PageHeader = ({title}) => {
+export const PageHeader = ({ title }) => {
   const navigate = useNavigate();
   return (
     <div className="grid grid-cols-3 items-center">
       <div>
-        <ArrowLeft
-          onClick={() => navigate(-1)}
-          className="h-7 w-7 cursor-pointer text-primary"
-        />
+        <button
+          type="button"
+          onClick={() =>
+            window.history.length > 1 ? navigate(-1) : navigate("/")
+          }
+        >
+          <ArrowLeft className="w-7 h-7 cursor-pointer text-primary" />
+        </button>
       </div>
 
       <h1 className="text-center text-xl font-semibold text-primary tracking-wide">
